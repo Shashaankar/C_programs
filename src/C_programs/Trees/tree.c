539d4e2f9167cd1ef95fe6a136cb66e8d6aaa200
Shashaankar Komirelly <skomirel@qca.qualcomm.com>
2015-03-29 22:50:38 -0700

    C_Prgms: remove incomplete sub tree

--- src/C_programs/Trees/tree.c
+++ src/C_programs/Trees/tree.c
@@ -60,7 +60,7 @@ int _full_btree_recursive(tnode *rootp) {
     int res=0;
 
     if (!rootp) {
-        return;
+        return 0;
     }
 
     if (rootp->left) {
@@ -130,12 +130,14 @@ void _check_full_binary_tree() {
     _tree_insert_left(root, 5, 0);
     _tree_insert_right(root, 6, 0);
     _tree_insert_right(root->right, 7, 0);
+    _tree_insert_left(root->left, 8, 0);
 
 
     /** logic to check for full binary tree */
     rootp = root;
     if(!rootp) {
         printf("Tree is empty \n");
+        printf("NULL Trees are full binary trees \n");
     }
 
     out = _full_btree_recursive(rootp);
@@ -146,10 +148,100 @@ void _check_full_binary_tree() {
 
 }
 
+/** Given A binary Tree, how do you remove all the half nodes (which has
+ * only one child)? Note leaves should not be touched as they have both
+ * children as NULL.*/
+
+/** @brief: recursive function called to adjust the half subtree nodes
+ * rootp -- > node pointer 
+ * parent_node -- > pointer to parent node */
+void _rem_half_nodes_recur(tnode *rootp, tnode *parent_node) {
+
+    tnode *delete_node=NULL;
+
+    if (!rootp) {
+        return;
+    }
+
+    if (rootp->left && rootp->right) {
+        _rem_half_nodes_recur(rootp->left, rootp);
+        _rem_half_nodes_recur(rootp->right, rootp);
+    }
+
+    if (rootp->left && !rootp->right && parent_node) {
+        /** Incomplete tree having only left sub tree */
+        delete_node = rootp;
+        printf("_rem_half: deleted key: %d, value: %d\n", delete_node->key, delete_node->value);
+
+        if (parent_node->left == delete_node) {
+            parent_node->left = delete_node->left;
+            free(delete_node);
+            _rem_half_nodes_recur(parent_node->left, parent_node);
+        } else {
+            parent_node->right = delete_node->left;
+            free(delete_node);
+            _rem_half_nodes_recur(parent_node->right, parent_node);
+        }
+
+        free(delete_node);
+        /** Now recurse on deleted node child */
+    }
+
+    if (!rootp->left && rootp->right && parent_node) {
+        /** Incomplete tree with only right sub tree */
+        delete_node = rootp;
+        printf("_rem_half: deleted key: %d, value: %d\n", delete_node->key, delete_node->value);
+        if (parent_node->left == delete_node) {
+            parent_node->left = delete_node->right;
+            free(delete_node);
+            _rem_half_nodes_recur(parent_node->left, parent_node);
+        } else {
+            parent_node->right = delete_node->right;
+            free(delete_node);
+            _rem_half_nodes_recur(parent_node->right, parent_node);
+        }
+    }
+
+    /** leaf nodes fall through */
+    return;
+}
+
+void _remove_half_nodes(){
+
+    tnode *rootp = NULL;
+
+    root = (tnode *)SRK_ALLOC(TNODE_SIZE);
+    root->key = 0;
+    root->value = 0;
+
+    /** Insert few nodes to test the feature */
+    _tree_insert_left(root, 7, 0);
+    _tree_insert_right(root, 5, 0);
+
+    _tree_insert_right(root->left,6, 0);
+    _tree_insert_left(root->left->right, 1, 0);
+    _tree_insert_right(root->left->right, 11, 0);
+
+
+    rootp = root;
+    /** Call a function to remove the half nodes */
+    if (!rootp) {
+        printf("Tree is empty\n");
+        return;
+    }
+    /** recursive function which deletes nodes and update pointers
+     * accordingly */
+    _rem_half_nodes_recur(root, NULL);
+
+    /** free nodes it will print the nodes also In free order */
+    _free_tnode(root);
+
+}
 
 void main() {
 
-    _check_full_binary_tree();
+   // _check_full_binary_tree();
+   _remove_half_nodes();
 }
 
 
