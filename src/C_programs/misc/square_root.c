28f2b29d2c2bc9009c4ac59269413a97ab345b07
Shashaankar Komirelly <skomirel@qca.qualcomm.com>
2015-10-10 02:20:34 -0700

    misc: C program sqrt corrections

--- src/C_programs/misc/square_root.c
+++ src/C_programs/misc/square_root.c
@@ -18,19 +18,38 @@
 
 int _get_squareroot(int x) {
 
-    int cnt = 0;
+    int mid_square = 0, mid =1;
+    int start = 1, end = 1, res = 1;
 
-    if (x <= 0) {
-        printf("\n Invalid number for square root");
-        return cnt;
+    if (x == 1 || x== 0) {
+        return x; 
+    } else if (x < 0) {
+        printf("\n Imaginary root, invalid number ");
+        return -1;
     }
 
-    while (x && x!= 1) {
-        x = x>>1;
-        cnt++;
+    start = 1;
+    end = x/2;
+    mid_square = 1;
+    /** square root of x is always less than x/2 for all integer x >= 1 */
+    while(start <= end) {
+
+        mid = (start + end)/2;
+        mid_square = mid * mid;
+
+        if (mid_square == x) {
+            return mid;
+        }
+        if (mid_square < x) {
+            start = mid+1;
+            res = mid;
+        } else {
+            end = mid-1;
+        }
     }
 
-    return cnt;
+    return res;
+
 }
 
 /** Belwo progrm prints square root of integers from 1 - 10 */
@@ -38,7 +57,7 @@ void main() {
 
     int i = 0;
 
-    for (i=0; i < 100; i++) {
+    for (i=0; i < 10000; i++) {
 
         printf("\nsquare root of %d:=%d", i, _get_squareroot(i));
     }
